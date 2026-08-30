7e676f22de4fa716ae7921be13fb78adf4cf3076
shamsy85 <SHAMSEERKANDATHIL@GMAIL.COM>
2026-08-30 21:04:05 +0530

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -69,7 +69,6 @@ def update_nifty_data():
     max_supply = round(atm_strike + (ce_close + pe_close), 2) if atm_strike else 0.0
     max_demand = round(atm_strike - (pe_close + ce_close), 2) if atm_strike else 0.0
 
-    # Sniper 1: Nearest 100 Rounding baseline based on ATM strike
     sniper_atm_100 = int(round(atm_strike / 100.0) * 100) if atm_strike else 0
     s1_strike = sniper_atm_100
     otm_ce_s1 = s1_strike + 100
@@ -78,7 +77,6 @@ def update_nifty_data():
     otm_ce_val_s1 = round(ce_close * 0.5, 2)
     otm_pe_val_s1 = round(pe_close * 0.5, 2)
 
-    # Sniper 2: Sniper 1 strike minus 50 points (e.g., 24200 - 50 = 24150)
     s2_strike = sniper_atm_100 - 50 if sniper_atm_100 else 0
     otm_ce_s2 = s2_strike + 100
     otm_pe_s2 = s2_strike - 100
